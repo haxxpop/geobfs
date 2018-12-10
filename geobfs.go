00de3bb3ee39f03292b3ad930b81ebde855789a5
Suphanat Chunhapanya <haxx.pop@gmail.com>
2018-12-11 05:43:37 +0700

    geobfs: Deobfuscation on the server side.
    The server will deobfuscate the input stream and put a result in the
    output stream.

--- geobfs.go
+++ geobfs.go
@@ -1,10 +1,13 @@
 package geobfs
 
 import (
+  "bufio"
   "crypto/rand"
   "fmt"
   "io"
   "math/big"
+  "strconv"
+  "strings"
 )
 
 const (
@@ -18,6 +21,8 @@ const (
   LONGITUDE_MAX = 180
   // The size of sample space used in noise randomness in the obfuscation.
   NOISE_GRANULARITY = 10000
+  // The size of sample space used in longitude randomness in the obfuscation.
+  LONGITUDE_GRANULARITY = 100000000
 )
 
 // Read all bytes from src, obfuscate them, and write them to dst.
@@ -55,17 +60,36 @@ func Obfuscate(dst io.Writer, src io.Reader) error {
   }
 }
 
+// Read all lines from src, deobfuscate them, and write them to dst.
 func Deobfuscate(dst io.Writer, src io.Reader) error {
-  // Allocate a buffer to store the read bytes.
-  read_buf := make([]byte, DEFAULT_BUFFER_LEN)
+  bufreader := bufio.NewReader(src)
   for {
-    nr, er := src.Read(read_buf)
-    if er != nil  {
-      // If it is just an EOF, no need to return error.
-      if er == io.EOF {
-        return nil
-      }
+    // Read each line of the stream.
+    line, er := bufreader.ReadString('\n')
+    if er != nil && er != io.EOF {
       return er
+    }
+    output, err := deobfuscateLine(line)
+    if err != nil {
+      return err
+    }
+
+    // Write the deobfuscated bytes to dst.
+    nw, ew := dst.Write([]byte{ output })
+    if ew != nil {
+      return ew
+    }
+    // If this happens, the OS somehow cannot write all the bytes but fail
+    // to return an explicit error. We should return an error instead of
+    // retrying to write to the destination stream because that can cause
+    // an infinite loop.
+    if nw != 1 {
+      return io.ErrShortWrite
+    }
+
+    // If we already reach the end of the stream, return.
+    if er == io.EOF {
+      return nil
     }
   }
 }
@@ -90,4 +114,43 @@ func obfuscateLine(val byte) string {
   longitude := float64(random.Int64()) / LONGITUDE_GRANULARITY *
                                                   float64(rng) + LONGITUDE_MIN
   return fmt.Sprintf("geo:%f,%f\n", latitude, longitude)
+}
+
+// Return a byte slice deobfuscated from the line.
+func deobfuscateLine(line string) (byte, error) {
+  // Trim a line.
+  line = strings.Trim(line, " \n")
+  // Trim a prefix.
+  coordinate := strings.TrimPrefix(line, "geo:")
+  // Split for a latitude and longitude.
+  slice := strings.Split(coordinate, ",")
+  if len(slice) != 2 {
+    return 0, fmt.Errorf("a line %s is malformed", line)
+  }
+
+  // Parse for latitude.
+  latitude, err := strconv.ParseFloat(slice[0], 64)
+  if err != nil {
+    return 0, err
+  }
+  if latitude < LATITUDE_MIN || latitude > LATITUDE_MAX {
+    return 0, fmt.Errorf("a latitude %f is not in a valid range", latitude)
+  }
+
+  // Parse for longitude.
+  // Even if we will discard the longitude, it is better to check that
+  // the longitude is valid.
+  longitude, err := strconv.ParseFloat(slice[1], 64)
+  if err != nil {
+    return 0, err
+  }
+  if longitude < LONGITUDE_MIN || longitude > LONGITUDE_MAX {
+    return 0, fmt.Errorf("a longitude %f is not in a valid range", longitude)
+  }
+
+  // Deobfuscate a byte from a latitude.
+  rng := LATITUDE_MAX - LATITUDE_MIN
+  step := float64(rng) / 256
+  // Casting float64 to byte will remove noise from the latitude.
+  return byte((latitude - float64(LATITUDE_MIN)) / step), nil
 }
